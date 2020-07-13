8b5de5e1757561cacfb480e56281fef6f885bf06
鎌田正聖 <masatoshi@masa-macbook-pro.local>
2020-07-13 22:04:48 +0900

    done a

--- src/bin/a.rs
+++ src/bin/a.rs
@@ -4,5 +4,15 @@ use proconio::{fastout, input};
 #[fastout]
 fn main() {
     input! {
+        n: usize,
+        s: Chars,
+        t: Chars,
     }
+    for i in 0..n {
+        if s[i..] == t[..n - i] {
+            println!("{}", i + n);
+            return;
+        }
+    }
+    println!("{}", n * 2);
 }
